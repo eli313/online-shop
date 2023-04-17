60a7595230ee474595a213cb8c9578e822aea5e3
M.Hassan <ali.ahmadi3676@gmail.com>
2023-04-17 19:15:58 +0430

    with circular structure problem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import React from 'react';
+import { Routes , Route , Navigate } from 'react-router-dom';
 
+// Components 
 import Cards from './Components/Cards';
 import NavBar from './Components/NavBar';
-import ContextProvider from './context/contextProvider';
+import HomePage from './Components/HomePage';
+import Cart from "./Components/Cart"
 
 // Context
 import HandlersProvider from './context/HandlersProvider';
+import ContextProvider from './context/contextProvider';
+
 function App() {
   return (
     <ContextProvider>
       <HandlersProvider>
         <NavBar/>
-        <Cards/>
+        <Routes>
+          <Route path='/homepage' element={<HomePage/>} />
+          <Route path='/products' element={<Cards/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/*' element={<Navigate to={<HomePage/>} />} />
+        </Routes>
       </HandlersProvider>
     </ContextProvider>
   );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,12 +6,15 @@ const NavBar = () => {
         <div className={styles.navbar} >
             <div className={styles.navItems}>
                 <div>
-                    <span className={styles.navItem}>Catagories</span>
-                    <span className={styles.navItem}>About</span>
+                    <span className={styles.navItem}>Home</span>
+                    <span className={styles.navItem}>Products & Catagories</span>
                 </div>
                 <div>
-                    <span className={styles.navItem}>Cart</span>
-                    <span className={styles.navItem}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+                    <span className={styles.navItem}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+</svg>
+</span>
+                    <span className={styles.navItem}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" class="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 </svg>
 </span>
